5c65049976da1c66b50ff8519d6f2ca835c0062a
k_zakarias <k_zakarias@hotmail.com>
2016-11-17 02:36:17 -0500

    bug fixing on stock controller

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -2,7 +2,7 @@ var init_stock_lookup;
 
 init_stock_lookup = function(){
   $('#stock-lookup-form').on('ajax:success', function(event, data, status){
-    $('stock-lookup').replaceWith(data);
+    $('#stock-lookup').replaceWith(data);
     init_stock_lookup();
   });
   $('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error){
