ce6244e5f6c76377d18d9030a55157aaa91e29ca
k_zakarias <k_zakarias@hotmail.com>
2016-11-18 15:28:56 -0500

    added spinner to my portfolio

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -6,11 +6,20 @@ init_stock_lookup = function(){
     init_stock_lookup();
   });
   $('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error){
+    hide_spinner();
     $('#stock-lookup-results').replaceWith(' ');
     $('#stock-lookup-errors').replaceWith('Stock was not found!');
     
   });
 }
+
+$('#stock-lookup-form').on('ajax:before', function(event, data, status){
+  show_spinner();
+});
+
+$('#stock-lookup-form').on('ajax:after', function(event, data, status){
+  hide_spinner();
+});
 
 $(document).ready( function() {
     init_stock_lookup();
